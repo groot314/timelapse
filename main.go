d4b35b9a9dc90ebf63178f1f6a7956cdc2559874
Geoffrey <groot314@gmail.com>
2025-09-14 14:22:07 -0400

    remove requirement for start flag, and adding progress output

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/groot314/timelapse/internal/helpers"
+	"github.com/groot314/timelapse/internal/progress"
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
@@ -13,14 +15,31 @@ func main() {
 	startNumber := flag.String("s", "", "Start number for photos.")
 	outputName := flag.String("o", "", "Output name.")
 	flag.Parse()
-	if *input == "" || *startNumber == "" || *outputName == "" {
+	if *input == "" || *outputName == "" {
 		fmt.Println("Need to input required flags. -i <input_dir> -s <start_number_index> -o <output_file_name>")
 		flag.Usage()
 		os.Exit(1)
 	}
-	err := ffmpeg.Input(*input+"/DSC%05d.JPG", ffmpeg.KwArgs{"framerate": "24", "start_number": *startNumber}).
+	if *startNumber == "" {
+		*startNumber, _ = helpers.FindPhotoStart(*input)
+	}
+
+	ffmpegInput := *input + "/DSC%05d.JPG"
+	ffmpegInputArgs := ffmpeg.KwArgs{"framerate": "24", "start_number": *startNumber}
+	a, err := ffmpeg.Probe(ffmpegInput, ffmpegInputArgs)
+	if err != nil {
+		panic(err)
+	}
+	totalDuration, err := progress.ProbeDuration(a)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ffmpeg.Input(ffmpegInput, ffmpegInputArgs).
 		Output("./"+*outputName+".mp4", ffmpeg.KwArgs{"c:v": "libx264", "pix_fmt": "yuv420p"}).
-		OverWriteOutput().ErrorToStdOut().Run()
+		GlobalArgs("-progress", "unix://"+progress.ProgressTempSock(totalDuration)).
+		OverWriteOutput().Run()
+	// OverWriteOutput().ErrorToStdOut().Run()
 	if err != nil {
 		fmt.Println("Error with ffmpeg:", err)
 	}
